aca59ea4df487fae269435edf538262c29ca8763
vumanhtien <tienvmgch17404@fpt.edu.vn>
2019-08-19 11:39:58 +0700

    con

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+    // $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
+    // echo "done";
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+                                      "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                                      $db["host"],
+                                      $db["port"],
+                                      $db["user"],
+                                      $db["pass"],
+                                      ltrim($db["path"], "/")
+                                      ));
+    
+    $sql = "SELECT studentname, course FROM registercourse";
+    $stmt = $pdo->prepare($sql);
+    //Thiết lập kiểu dữ liệu trả về
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $resultSet = $stmt->fetchAll();
+    
+    ?>
+<ul>
+<?php
+    foreach ($resultSet as $row) {
+        echo '<li>' .
+        $row['studentname'] . ' --' . $row['course']
+        . '</li>';
+    }
+    ?>
+</ul>
+
+</body>
+</html>
